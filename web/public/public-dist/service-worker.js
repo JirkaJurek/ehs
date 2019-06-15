74cd6fb49f8a1cf1cb5d2b5216f86c3b1b629267
Michal Rosecky <mr.michal.rosecky@gmail.com>
2019-06-15 18:21:29 +0200

    service-worker

--- web/public/public-dist/service-worker.js
+++ web/public/public-dist/service-worker.js
@@ -1,51 +1,17 @@
-importScripts("/public-dist/precache-manifest.bbf2df97efd42f6824139f2570a2b6fe.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+workbox.setConfig({
+  debug: true
+});
 
-workbox.core.setCacheNameDetails({prefix: "ehs-public"});
+// This will trigger the importScripts() for workbox.strategies and its dependencies:
+workbox.loadModule('workbox-strategies');
 
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-// workbox.setConfig({
-//   debug: true
-// });
-
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "images",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
-      })
-    ]
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp(".*/public"),
-  workbox.strategies.networkFirst({
-    cacheName: "public"
-  })
-);
-workbox.routing.registerRoute(
-  /\.\*\/languages$/,
-  workbox.strategies.networkFirst({
-    cacheName: "languages"
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
-  workbox.strategies.cacheFirst({
-    cacheName: "googleapis",
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 30
-      })
-    ]
-  })
-);
-
+self.addEventListener('fetch', (event) => {
+  console.log(event)
+  if (event.request.url.endsWith('.css')) {
+    // Referencing workbox.strategies will now work as expected.
+    const cacheFirst = new workbox.strategies.CacheFirst();
+    event.respondWith(cacheFirst.makeRequest({request: event.request}));
+  }
+});
