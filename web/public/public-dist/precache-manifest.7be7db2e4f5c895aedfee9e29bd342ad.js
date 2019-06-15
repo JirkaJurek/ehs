9018b99b6d883a2d12ea3fba0914d4c081a9de3b
Michal Rosecky <mr.michal.rosecky@gmail.com>
2019-06-15 15:08:04 +0200

    service-worker

---
+++ web/public/public-dist/precache-manifest.7be7db2e4f5c895aedfee9e29bd342ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3dea7c111fcc14ee187576bc30a6f5f",
+    "url": "/public-dist/style.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/public-dist/robots.txt"
+  },
+  {
+    "revision": "8a7ac561f105bb1e8750",
+    "url": "/public-dist/js/chunk-vendors.5ade17e1.js"
+  },
+  {
+    "revision": "3742e9cc0d8918e59ea3",
+    "url": "/public-dist/js/app.b70178df.js"
+  },
+  {
+    "revision": "c069222c3e2558badd1f1394e46020d3",
+    "url": "/public-dist/index.html"
+  },
+  {
+    "revision": "3742e9cc0d8918e59ea3",
+    "url": "/public-dist/css/app.6b1057dd.css"
+  }
+];
---
+++ web/public/public-dist/service-worker.js
@@ -0,0 +1,51 @@
+importScripts("/public-dist/precache-manifest.7be7db2e4f5c895aedfee9e29bd342ad.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+
+workbox.core.setCacheNameDetails({prefix: "ehs-public"});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+// workbox.setConfig({
+//   debug: true
+// });
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "images",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp(".*/public"),
+  workbox.strategies.networkFirst({
+    cacheName: "public"
+  })
+);
+workbox.routing.registerRoute(
+  /\.\*\/languages$/,
+  workbox.strategies.networkFirst({
+    cacheName: "languages"
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+  workbox.strategies.cacheFirst({
+    cacheName: "googleapis",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+);
+
